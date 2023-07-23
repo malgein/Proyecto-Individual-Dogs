a5ba52836a58038863967478d9d67d9c15a17fe2
wilmer pocaterra <malgein17@gmail.com>
2023-07-23 01:25:58 -0400

    feat, styled(Error404)pagina 404 creada y estilizada

--- client/src/components/pages/Error/Error404.jsx
+++ client/src/components/pages/Error/Error404.jsx
@@ -1,8 +1,25 @@
+//Dependencias
 import React from 'react'
+import { Link } from 'react-router-dom'
+//Estilos
+import styles from './Error404.module.css'
+import Dog from './Dog-Transparent-Background.png'
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <div className={styles.error}>
+      <div className={styles.sky}>
+        <h2><span>4</span><span>0</span><span>4</span></h2>
+        <div className={styles.grass}></div>
+        <img src={Dog} alt='dog worried' className={styles.dog}/>
+      </div>
+      <div className={styles.field}>
+        <h2>Opps... Parece que te perdiste.
+          No encontras nada aqui  
+        </h2>
+        <Link className={styles.linkError} to='./home'>Volver a la Home</Link>
+      </div>
+    </div>
   )
 }
 
